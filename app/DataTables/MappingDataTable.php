2821eb4b6f964ded932da04c4f77c7032609b071
ShubhuCoder <shubhucoder@gmail.com>
2025-05-28 23:02:40 +0530

    luve server setting issue resolve

--- app/DataTables/MappingDataTable.php
+++ app/DataTables/MappingDataTable.php
@@ -7,7 +7,7 @@ use Yajra\DataTables\Html\Button;
 use Yajra\DataTables\Html\Column;
 use Yajra\DataTables\Services\DataTable;
 
-class MappingDatatable extends DataTable
+class MappingDataTable extends DataTable
 {
     public function dataTable($query)
     {
